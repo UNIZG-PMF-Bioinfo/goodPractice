dadcaf436f63a2ea811a95cf22bbc6d6d20ea273
Maja Kuzman <37149324+MaKuzman@users.noreply.github.com>
2020-03-21 12:21:40 +0100

    Create minimapShortReadsToGenome.sh

---
+++ mapping/minimapShortReadsToGenome.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# job name
+#PBS -N mmSR
+## when and who to mail - change if you want mail and remove one starting # 
+##PBS -m bea
+## PBS -M your@mail.com
+# queue:
+#PBS -q MASTER
+#PBS -l select=1:ncpus=12:mem=80G
+
+cd $PBS_O_WORKDIR
+
+##------------------------------------------------------------------#
+## script expects input names for genome and nanopores.fasta files!
+## call it with qsub -v genome=genomename.fasta,nano=nanopores.fasta minimapTranscriptsToGenome.sh
+##------------------------------------------------------------------#
+
+minimap=/common/WORK/mfabijanic/programs/miniconda3/bin/minimap2
+OUTn=${reads%%1.fasta}
+OUTg=${genome%%.fasta}
+OUT=${OUTn}to${OUTg}
+
+$minimap -x sr -Y -K 10000M -t 12 $genome $reads1 $reads2 -o ${OUT}.paf
+$minimap -ax sr -Y -K 10000M -t 12 $genome $reads1 $reads2 -o ${OUT}.sam
+
+sambamba=/common/WORK/fhorvat/programi/sambamba/sambamba
+$sambamba view -t 12 -f bam -S -o ${OUT}.bam ${OUT}.sam
+$sambamba sort -m 80GB --tmpdir tmp -t 12 ${OUT}.bam
+$sambamba flagstat -t 12 ${OUT}.sorted.bam > ${OUT}.flagstat.txt
+
+rm ${OUT}.sam ${OUT}.sam 
+rm -r tmp
+chmod 444 ${OUT}*
