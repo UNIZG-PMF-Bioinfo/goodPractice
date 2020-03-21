2d2a49f002390b81ab72b04e10fd211dca6f5e30
Maja Kuzman <37149324+MaKuzman@users.noreply.github.com>
2020-03-21 11:37:49 +0100

    Create minimapTranscriptsToGenome.sh
    script expects input names for genome and transcript .fasta files!
    maps transcript to genome and creates .paf , .sorted.bam +bai files, and flagstat info.

---
+++ mapping/minimapTranscriptsToGenome.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# job name
+#PBS -N mm
+## when and who to mail - change if you want mail and remove one starting # 
+##PBS -m bea
+## PBS -M your@mail.com
+# queue:
+#PBS -q MASTER
+#PBS -l select=1:ncpus=12:mem=80G
+
+cd $PBS_O_WORKDIR
+
+##------------------------------------------------------------------#
+## script expects input names for genome and transcript .fasta files!
+## call it with qsub -v genome=genomename.fasta,transcripts=transcripts.fasta minimapTranscriptsToGenome.sh
+##------------------------------------------------------------------#
+
+minimap=/common/WORK/mfabijanic/programs/miniconda3/bin/minimap2
+OUTt=${transcripts%%.fasta}
+OUTg=${genome%%.fasta}
+OUT={OUTt}to{$OUTg}
+
+$minimap -x splice -K 10000M -t 12 $genome transcripts -o ${OUT}.paf
+$minimap -ax splice -K 10000M -t 12 $genome Trinity_day1.fasta -o ${OUT}.sam
+
+sambamba=/common/WORK/fhorvat/programi/sambamba/sambamba
+$sambamba view -t 12 -f bam -S -o ${OUT}.bam ${OUT}.sam
+$sambamba sort -m 80GB --tmpdir tmp -t 12 ${OUT}.bam
+$sambamba flagstat -t 12 ${OUT}.sorted.bam > ${OUT}.flagstat.txt
+
+rm ${OUT}.sam ${OUT}.sam 
+rm -r tmp
