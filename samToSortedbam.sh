b46a6a5a7a6c74e0d789a261961ea7d7fe6f88af
Maja Kuzman <37149324+MaKuzman@users.noreply.github.com>
2020-03-21 11:06:45 +0100

    Create samToSortedbam.sh

---
+++ samToSortedbam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N samtosortedbam
+#PBS -m ea
+#PBS -M youremail@mail.com
+#PBS -q MASTER
+#PBS -l select=1:ncpus=6:mem=120G
+cd $PBS_O_WORKDIR
+
+SAMBAMBA=/common/WORK/fhorvat/programi/sambamba/sambamba
+$SAMBAMBA view -f bam -F "" -o ${IN}.bam -t 6 -S ${IN}.sam
+$SAMBAMBA sort -m 100GB --tmpdir tmp -t 6 ${IN}.bam
+$SAMBAMBA flagstat -t 10 ${IN}.sorted.bam > ${IN}.flagstat.txt
+rm ${IN}.sam ${IN}.bam
+rm -r tmp
